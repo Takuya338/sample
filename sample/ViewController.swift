a4164171ee33183486063e9d26ecfe2164168745
TakuyaGoto <ggyabcjkl@gmail.com>
2024-12-22 12:56:38 +0000

    First Settings

---
+++ sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by ec2-user on 12/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
